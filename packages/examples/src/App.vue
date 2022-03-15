5dcc418839768375c2afe78cf3953d98db560ec4
Hunter <hunterliu1003@gmail.com>
2022-03-15 12:41:29 +0800

    feat(examples): add basic examples

---
+++ packages/examples/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { TheComponent } from '@vue-final/the-component'
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <TheComponent msg="Hello Vue 3 + Vite" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
