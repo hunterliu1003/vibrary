76e090a4f86edebae95903a1666cc4eebd604d9b
Hunter <hunterliu1003@gmail.com>
2022-07-03 19:55:12 +0800

    feat: add script rename-lib

---
+++ scripts/renameLibrary.js
@@ -0,0 +1,24 @@
+const prompt = require('prompt')
+const replace = require('replace-in-file')
+
+prompt.start()
+
+;(async () => {
+  const oldName = require('../packages/lib/package.json').name
+  const { newName } = await prompt.get({
+    properties: {
+      newName: {
+        pattern: /^(@[a-z0-9-~][a-z0-9-._~]*\/)?[a-z0-9-~][a-z0-9-._~]*$/,
+        description: '\nEnter the library name you want (e.g: my-library-name)',
+        message: 'Invalid name /^(@[a-z0-9-~][a-z0-9-._~]*\/)?[a-z0-9-~][a-z0-9-._~]*$/',
+        required: true,
+      },
+    },
+  })
+
+  replace({
+    files: '**/*',
+    from: oldName,
+    to: newName,
+  })
+})()
